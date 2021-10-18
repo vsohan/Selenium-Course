9990a67ac2f03302c5e3ca818e48d63ad914867a
vsohan <90085202+vsohan@users.noreply.github.com>
2021-10-18 23:01:05 +0530

    Add files via upload

---
+++ src/test/java/com/qascript/FirstTest.java
@@ -0,0 +1,28 @@
+package com.qascript;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FirstTest {
+
+
+    @Test
+    public void OpenBrowser()  {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("disable-gpu");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+        System.out.println("Title of the page is: " + driver.getTitle());
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
+    }
+
+
+}
+
+
