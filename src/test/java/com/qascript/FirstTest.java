37617c07262b9892abd75b8baccce408e20e198a
vsohan <90085202+vsohan@users.noreply.github.com>
2021-10-18 23:43:07 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,28 +1,27 @@
-package com.qascript;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-public class FirstTest {
-
-
-    @Test
-    public void OpenBrowser()  {
-        WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.addArguments("disable-gpu");
-        driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
-    }
-
-
+import org.openqa.selenium.By;  
+import org.openqa.selenium.WebDriver;  
+import org.openqa.selenium.chrome.ChromeDriver;  
+  
+public class First {  
+  
+    public static void main(String[] args) {  
+        
+    // declaration and instantiation of objects/variables  
+    System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");  
+    WebDriver driver=new ChromeDriver();  
+      
+// Launch website  
+    driver.navigate().to("http://www.google.com/");  
+          
+    // Click on the search text box and send value  
+    driver.findElement(By.id("lst-ib")).sendKeys("javatpoint tutorials");  
+          
+    // Click on the search button  
+    driver.findElement(By.name("btnK")).click();  
+      
+    }  
+  
+}  
 }
 
 
